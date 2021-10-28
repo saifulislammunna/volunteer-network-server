3028d2916004ce77fbfa6105e046d6ef7ac62b0d
saifulislammunna <saifulmunna28@gmail.com>
2021-10-28 11:42:20 +0600

    added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config()
 
 const app = express()
-const port = 5000
+const port =  process.env.PORT || 5000
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u6dke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -16,6 +22,28 @@ async function run(){
     try{
         await client.connect();
         console.log('database connected');
+        const database = client.db('volunteerNetwork');
+        const serviceCollection = database.collection('services');
+
+        // POST API
+        // app.post('/services', async(req,res) =>{
+        //     const service = 
+        //         { "img":"https://i.ibb.co/HT3fM94/child-Support.png",
+        //         "name":"Child Support"
+        //     }
+
+        //     const result = await servicesCollection.insertOne(service);
+        //     console.log(result);
+
+            
+        // })
+
+        // GET Products API 
+        app.get('/services', async(req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
 
     }
     finally{
